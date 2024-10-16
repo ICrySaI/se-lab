43d69cbe239717cf7e26f0ce7482c08c0ee9678a
unknown <184546@BME.HU>
2024-10-16 17:25:04 +0200

    leírás arról hogy a generator mire való

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,7 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  private Random generator = new Random();
+  private Random generator = new Random(); //random generator for the fire method to use
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
